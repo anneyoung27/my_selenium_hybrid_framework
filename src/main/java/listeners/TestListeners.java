c9f175b2001f6ea0d6977ec662d3128d4abe1d6d
Injas Mahendra Berutu <injasmahendra123@gmail.com>
2025-11-12 22:22:24 +0700

    update

---
+++ src/main/java/listeners/TestListeners.java
@@ -0,0 +1,96 @@
+package listeners;
+
+import com.aventstack.extentreports.Status;
+import helper.CaptureHelper;
+import helper.PropertiesHelper;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import reports.AllureManager;
+import reports.ExtentReportManager;
+import reports.ExtentTestManager;
+import utils.LogUtils;
+
+public class TestListeners implements ITestListener {
+    public String getTestName(ITestResult result){
+        return result.getTestName() != null ? result.getTestName() : result.getMethod().getConstructorOrMethod().getName();
+    }
+
+    public String getTestDescription(ITestResult result){
+        return result.getMethod().getDescription() != null ? result.getMethod().getDescription() : getTestName(result);
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+        LogUtils.info("⭐\uFE0F ********* START TESTING " + context.getName() + " *********");
+        PropertiesHelper.loadAllFiles();
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        LogUtils.info("⭐\uFE0F ********* END TESTING " + context.getName() + " *********");
+        ExtentReportManager.getExtentReports().flush();
+    }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        LogUtils.info("➡\uFE0F Starting test case " + getTestName(result));
+
+        ExtentTestManager.saveToReport(getTestName(result), getTestDescription(result));
+
+        if(PropertiesHelper.getValue("RECORD_VIDEO").equals("true")){
+            CaptureHelper.startRecord(getTestName(result));
+        }
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        LogUtils.info("✅ Test case " + getTestName(result) + " passed.");
+
+        //Extent Report
+        ExtentTestManager.logMessage(Status.PASS, getTestName(result) + " is passed.");
+
+        if(PropertiesHelper.getValue("SCREENSHOT_STEP_PASS").equals("true")) {
+            CaptureHelper.screenshot(getTestName(result));
+        }
+
+        if(PropertiesHelper.getValue("RECORD_VIDEO").equals("true")){
+            CaptureHelper.stopRecord();
+        }
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        LogUtils.error("❌ Test case " + getTestName(result) + " failed.");
+        LogUtils.error(result.getThrowable());
+
+        //Extent Report
+        ExtentTestManager.logMessage(Status.FAIL, result.getThrowable().toString());
+        ExtentTestManager.logMessage(Status.FAIL, getTestName(result) + " is failed.");
+
+        //Allure Report
+        //AllureManager.saveTextLog(iTestResult.getThrowable().toString());
+        //AllureManager.saveTextLog(iTestResult.getName() + " is failed.");
+
+        if(PropertiesHelper.getValue("SCREENSHOT_STEP_FAIL").equals("true")) {
+            CaptureHelper.screenshot(getTestName(result));
+            ExtentTestManager.addScreenshot(getTestName(result));
+            AllureManager.saveScreenshotPNG();
+        }
+
+        if(PropertiesHelper.getValue("RECORD_VIDEO").equals("true")){
+            CaptureHelper.stopRecord();
+        }
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        LogUtils.warn("\uD83D\uDD1C Test case " + getTestName(result) + " skipped.");
+        //Extent Report
+        ExtentTestManager.logMessage(Status.SKIP, result.getThrowable().toString());
+
+        if(PropertiesHelper.getValue("RECORD_VIDEO").equals("true")){
+            CaptureHelper.stopRecord();
+        }
+    }
+}
---
+++ src/test/java/common/BaseTest.java
@@ -0,0 +1,63 @@
+package common;
+
+import factory.DriverFactory;
+import helper.PropertiesHelper;
+import listeners.TestListeners;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+import utils.LogUtils;
+
+@Listeners(TestListeners.class)
+public class BaseTest {
+
+    @BeforeMethod
+    @Parameters({"browser"})
+    public void createDriver(@Optional("chrome") String browser) {
+        WebDriver driver;
+        if (PropertiesHelper.getValue("BROWSER") != null && !PropertiesHelper.getValue("BROWSER").isEmpty()){
+            driver = setUpDriver(PropertiesHelper.getValue("BROWSER"));
+        } else {
+            driver = setUpDriver(browser);
+        }
+        DriverFactory.setDriver(driver);
+    }
+
+    public WebDriver setUpDriver(String browser) {
+        WebDriver driver = switch (browser.toLowerCase()) {
+            case "firefox" -> initFireFoxDriver();
+            case "edge" -> initEdgeDriver();
+            default -> initChromeDriver();
+        };
+        DriverFactory.setDriver(driver);
+        return driver;
+    }
+
+    private WebDriver initChromeDriver() {
+        LogUtils.info("Launching Chrome browser...");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    private WebDriver initEdgeDriver() {
+        LogUtils.info("Launching Edge browser...");
+        WebDriver driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    private WebDriver initFireFoxDriver() {
+        LogUtils.info("Launching FireFox browser...");
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    @AfterMethod
+    public void closeDriver(){
+        DriverFactory.quit();
+    }
+}
