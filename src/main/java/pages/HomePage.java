6d020a949f19186dabe27ec0f09a781456ebc7eb
Injas Mahendra Berutu <injasmahendra123@gmail.com>
2025-11-15 14:54:07 +0700

    update

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,11 +1,45 @@
 package pages;
 
+import keywords.WebUI;
+import org.openqa.selenium.By;
+
 import java.util.Properties;
+
+import static keywords.WebUI.*;
 
 public class HomePage {
     Properties setUp;
 
     public HomePage(){}
 
-    String homeHeader;
+    String homeHeader = setUp.getProperty("HOME_HEADER");
+    String popularProductList = setUp.getProperty("POPULAR_PRODUCTS_SECTION");
+    String basketCount = setUp.getProperty("BASKET_COUNT");
+
+    public String getHomeHeader(){
+        waitForElementPresent(By.xpath(homeHeader));
+        return By.xpath(homeHeader).toString();
+    }
+
+    public int getCountOfPopularProducts(){
+        waitForElementPresent(By.xpath(popularProductList));
+        return getListWebElements(By.xpath(popularProductList)).size();
+    }
+
+    public String getPopularProductsName(){
+        waitForElementPresent(By.xpath(popularProductList));
+        return By.xpath(popularProductList).toString();
+    }
+
+    public void clickAddToBasketButton(int index){
+        String addToBasketButton = "//div[@class='row text-center']/div//a[@data-id='"+index+"']";
+        waitForElementClickable(By.xpath(addToBasketButton));
+        clickElement(By.xpath(addToBasketButton));
+    }
+
+    public int getCountOfAddToBasketButtons(){
+        waitForElementPresent(By.xpath(basketCount));
+        return Integer.parseInt(getWebElement(By.xpath(basketCount)).toString());
+    }
+
 }
--- src/main/java/pages/ProductPage.java
+++ src/main/java/pages/ProductPage.java
@@ -1,11 +1,43 @@
 package pages;
 
+import org.openqa.selenium.By;
+
 import java.util.Properties;
+
+import static keywords.WebUI.*;
 
 public class ProductPage {
     Properties setUp;
 
     public ProductPage(){}
+
+    String productHeader = setUp.getProperty("PRODUCT_HEADER");
+    String productNameList = setUp.getProperty("PRODUCT_LIST");
+
+
+    public String getProductHeader(){
+        waitForElementPresent(By.xpath(productHeader));
+        return By.xpath(productHeader).toString();
+    }
+
+    public int getCountOfProducts(){
+        waitForElementPresent(By.xpath(productNameList));
+        return getListWebElements(By.xpath(productHeader)).size();
+    }
+
+    public String getProductName(int index){
+        waitForElementPresent(By.xpath(productNameList));
+        return getListWebElements(By.xpath(productNameList)).get(index).getText();
+    }
+
+    public void clickAddToBasket(int index){
+        String addToBasketButton = "//div[@class='container']//div//a[@data-id='"+index+"']";
+        waitForElementClickable(By.xpath(addToBasketButton));
+        clickElement(By.xpath(addToBasketButton));
+    }
+
+
+
 
 
 }
